95cc3ede06998fc6a2c02729c7f48651d63b65fa
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-10-04 15:50:32 -0300

    Config
    Se intalaron dependencias de test y se creo un archivo env para el manejo de dependencias

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,7 +1,11 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+
+//importar variables desde .env
+dotenv.config();
 
 //'database','username','password'
-export const sequelize = new Sequelize('pets','root','',{
+export const sequelize = new Sequelize('pets',process.env.DB_USER, process.env.DB_PASSWORD,{
     host:'localhost',
     dialect: 'mysql'
 });
@@ -10,5 +14,7 @@ try {
     await sequelize.authenticate();
     console.log('Connection has been establish successfully');
 } catch (error) {
+    console.log(process.env.DB_USER)
+    console.log(process.env.DB_PASSWORD)
     console.log('unable to connect to database:', error);
 }
