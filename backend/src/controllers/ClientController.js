bee4c9b1c589e6d9a1d3087f362808cec0198c3a
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-10-09 14:48:01 -0300

    Hash password in Client model

--- backend/src/controllers/ClientController.js
+++ backend/src/controllers/ClientController.js
@@ -1,5 +1,5 @@
 import { Client } from '../models/Client.js';
-import bcrypt from 'bcrypt';
+
 
 export const getClient = async ( req, res ) =>{
     try {
@@ -9,6 +9,7 @@ export const getClient = async ( req, res ) =>{
         return res.status( 500 ).json({ message: error }); 
     }
 }
+
 export const getOneClient = async ( req, res ) =>{
     try {
         const { id } = req.params;
@@ -18,21 +19,23 @@ export const getOneClient = async ( req, res ) =>{
         return res.status( 500 ).json({ message: error }); 
     }
 }
+
 export const createClient = async (req, res) =>{
     try {
         const { name, phone, address, email, pass, profile_picture } = req.body;
-        
-        const passHashed = await bcrypt.hash(pass, 10);
 
         const clientExist = await Client.findOne({ where: { email } });
-        if(clientExist) return res.status( 400 ).json({ message: 'El usuario ya existe' });
 
-        const createClient = await Client.create({ name, phone, address, email, pass: passHashed, profile_picture });
+        if(clientExist) return res.status( 400 ).json({ message: 'El usuario ya existe' });
+        
+        const createClient = await Client.create({ name, phone, address, email, pass, profile_picture });
+
         return res.status( 200 ).json({ result: createClient });
     } catch ( error ) {
         return res.status( 500 ).json({ message: error.message });
     }
 }
+
 export const updateClient = async (req, res) =>{
     try {
         const { id } = req.params;
@@ -44,6 +47,7 @@ export const updateClient = async (req, res) =>{
         return res.status( 500 ).json({ message: error });
     }
 }
+
 export const deleteClient = async (req, res) =>{
     try {
         const { id } = req.params;
