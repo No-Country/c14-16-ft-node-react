a68d0f59dd81477d210efe54b6c2c690819d55a7
Emanuel Valente <emavalente.dev@gmail.com>
2023-10-14 13:30:25 -0300

    12/10/23: vista Sobre Nosotros primer maquetado

--- frontend/src/components/pages/about/About.jsx
+++ frontend/src/components/pages/about/About.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+import CardAbout from "./CardAbout";
 import DogCat from "/assets/bg-001.jpg";
 import Hero from "/assets/carrusel1.jpg";
 import "./About.css";
 
 const About = () => {
+  const texts = [
+    " Doggy’s House es la unión de distintas guarderías porteñas como “Ciudad mascotera”, “Guardería Canina Belgrano R”, “Moni Guardería Canina”, “Rock&dog guardería canina”, “Ruta canina” y muchas otras más; para que tus hijos peludos tengan siempre la mejor atención posible.",
+    "Doggy’s House nace de la iniciativa de un grupo de estudiantes de sistemas, viendo un problema en el mercado encontraron esta solución tanto para los dueños de las guarderías como para los dueños de facilitar la reserva y seguridad a la hora de pagar.",
+    "En Doogy’s house sabemos que la vida a veces puede complicarse. Nuestro objetivo es que viajes con tranquilidad y disfrutes de tus actividades favoritas sin la preocupación de quién cuidará a tu perro. Con la cobertura veterinaria (si esta incluida), las fotos y videos semanales que recibirás y los cuidadores verificados por el equipo de Doogy’s house, no volverás a tener dudas sobre dónde dejar a tu mascota cuando no te pueda acompañar",
+  ];
   const mainStyle = {
     backgroundImage: `url(${DogCat})`,
     backgroundSize: "cover",
@@ -20,58 +27,35 @@ const About = () => {
   return (
     <div>
       <header style={heroStyle}>
-        <div className="container mx-auto">
-          <h1 className="py-32 text-center text-white text-6xl">About Us</h1>
+        <div className="py-64 container mx-auto">
+          <h1 className=" text-center text-white text-6xl">Sobre Nosotros</h1>
         </div>
       </header>
       <main>
-        <section style={mainStyle} className="p-16">
-          <div className="container mx-auto">
-            <article className="p-24 mb-8 bg-[rgba(224,224,224,0.80)] rounded-xl">
-              <h3 className="mb-8 text-black font-bold text-4xl">
-                ¿Quienes somos?
-              </h3>
-              <p className="">
-                Doggy’s House es la unión de distintas guarderías porteñas como
-                “Ciudad mascotera”, “Guardería Canina Belgrano R”, “Moni
-                Guardería Canina”, “Rock&dog guardería canina”, “Ruta canina” y
-                muchas otras más; para que tus hijos peludos tengan siempre la
-                mejor atención posible.
-              </p>
-            </article>
-            <article className="p-24 mb-8 bg-[rgba(224,224,224,0.80)] rounded-xl">
-              <h3 className="mb-8 text-black font-bold text-4xl">
-                ¿De dónde nace la idea?
-              </h3>
-              <p>
-                Doggy’s House nace de la iniciativa de un grupo de estudiantes
-                de sistemas, viendo un problema en el mercado encontraron esta
-                solución tanto para los dueños de las guarderías como para los
-                dueños de facilitar la reserva y seguridad a la hora de pagar.
-              </p>
-            </article>
-            <article className="p-24 mb-8 bg-[rgba(224,224,224,0.80)] rounded-xl">
-              <h3 className="mb-8 text-black font-bold text-4xl">
-                ¿Por qué elegir Doggy’s House?
-              </h3>
-              <p>
-                En Doogy’s house sabemos que la vida a veces puede complicarse.
-                Nuestro objetivo es que viajes con tranquilidad y disfrutes de
-                tus actividades favoritas sin la preocupación de quién cuidará a
-                tu perro. Con la cobertura veterinaria (si esta incluida), las
-                fotos y videos semanales que recibirás y los cuidadores
-                verificados por el equipo de Doogy’s house, no volverás a tener
-                dudas sobre dónde dejar a tu mascota cuando no te pueda
-                acompañar
-              </p>
-            </article>
+        <section style={mainStyle} className="px-4 py-32 md:px-0">
+          <div className="flex flex-col gap-16 mx-auto">
+            <CardAbout
+              title="¿Quienes Somos?"
+              text={texts[0]}
+              styles="md:justify-end"
+            />
+            <CardAbout
+              title="¿De donde nace la idea?"
+              text={texts[1]}
+              styles="md:justify-start"
+            />
+            <CardAbout
+              title="¿Por qué elegir Doggy’s House?"
+              text={texts[2]}
+              styles="md:justify-end"
+            />
           </div>
         </section>
-        <section className="p-16 container mx-auto">
+        <section className="p-16 container flex flex-col items-center mx-auto">
           <h2 className="mb-16 text-4xl text-center font-bold">
             Doggy’s House en numeros
           </h2>
-          <article className="flex gap-16 justify-center">
+          <article className="mb-16 flex gap-16 justify-center">
             <div>
               <h4 className="text-4xl text-center font-bold">
                 <span>1537</span> días
@@ -91,6 +75,8 @@ const About = () => {
               <p>Reseñas de 5 estrellas</p>
             </div>
           </article>
+
+          <Link>Buscar Guardería</Link>
         </section>
       </main>
     </div>
---
+++ frontend/src/components/pages/about/CardAbout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CardAbout = ({ title, text, styles }) => {
+  return (
+    <div className={`w-full flex ${styles}`}>
+      <article className="md:w-[45%] p-8 bg-[rgba(224,224,224,0.70)] rounded-xl">
+        <h3 className="mb-8 text-black font-bold text-4xl">{title}</h3>
+        <p className="">{text}</p>
+      </article>
+    </div>
+  );
+};
+
+export default CardAbout;
