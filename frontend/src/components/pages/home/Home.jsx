d9718d744768460d91d84aed968efdda577406a1
Emanuel Valente <emavalente.dev@gmail.com>
2023-10-16 16:05:15 -0300

    16/10/23 - Manejo de busqueda principal sin mostrar resultados

--- frontend/src/components/pages/home/Home.jsx
+++ frontend/src/components/pages/home/Home.jsx
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './Home.css'
-
+import "./Home.css";
+import { SearchContext } from "../../../context/SearchContext";
 
 const Home = () => {
-  const [tipoAnimal, setTipoAnimal] = useState("Perro");
   const navigate = useNavigate();
-  const handleClick = () => { navigate('/search') }
 
+  const { handleSearchTerm } = useContext(SearchContext);
+
+  const userSearch = {
+    place: "",
+    petType: "",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (Object.values(userSearch).includes("")) {
+      alert("Todos los campos son obligatorios");
+      return;
+    }
+    handleSearchTerm(userSearch);
+    navigate("/search");
+  };
 
   return (
     <div className="bg-gray-100 py-8">
@@ -28,7 +43,9 @@ const Home = () => {
               alt="Imagen 1"
               className="w-full h-48 mx-auto rounded-lg"
             />
-            <h3 className="text-2xl font-semibold text-center m-4">Dog Working</h3>
+            <h3 className="text-2xl font-semibold text-center m-4">
+              Dog Working
+            </h3>
             <p className="mt-4">
               Nuestro equipo de expertos está dedicado al cuidado de tu mascota.
             </p>
@@ -41,7 +58,9 @@ const Home = () => {
               alt="Imagen 2"
               className="w-full h-48 mx-auto rounded-lg"
             />
-            <h3 className="text-2xl font-semibold text-center m-4">Dog Office</h3>
+            <h3 className="text-2xl font-semibold text-center m-4">
+              Dog Office
+            </h3>
             <p className="mt-4">
               Ofrecemos un ambiente seguro y divertido para que tu mascota se
               sienta como en casa.
@@ -55,7 +74,9 @@ const Home = () => {
               alt="Imagen 3"
               className="w-full h-48 mx-auto rounded-lg"
             />
-            <h3 className="text-2xl font-semibold text-center m-4">Dog Hotel</h3>
+            <h3 className="text-2xl font-semibold text-center m-4">
+              Dog Hotel
+            </h3>
             <p className="mt-4">
               Nos preocupamos por la salud y el bienestar de cada animalito que
               llega a nosotros.
@@ -66,52 +87,76 @@ const Home = () => {
         {/* Servicios incluidos */}
 
         <div className="p-4 text-center grid grid-cols-2 md:grid-cols-3 gap-4">
-          <h3 className="text-2xl font-bold text-center m-4 col-span-2 md:col-span-3">Servicios incluidos</h3>
+          <h3 className="text-2xl font-bold text-center m-4 col-span-2 md:col-span-3">
+            Servicios incluidos
+          </h3>
 
           <div className="bg-gray-400 rounded-lg shadow-md p-4 text-center">
             <p className="text-white font-bold py-2 px-4">Peluqueria</p>
-            <img src="./assets/pelu.png" alt="peluqueria" width={45} className="pt-2 mx-auto" />
+            <img
+              src="./assets/pelu.png"
+              alt="peluqueria"
+              width={45}
+              className="pt-2 mx-auto"
+            />
           </div>
 
           <div className="bg-gray-400 rounded-lg shadow-md p-4 text-center">
-            <p className="text-white font-bold py-2 px-4" >
-              Dietas especiales
-            </p>
-            <img src="./assets/food.png" alt="Foot dog" width={70} className=" mx-auto" />
+            <p className="text-white font-bold py-2 px-4">Dietas especiales</p>
+            <img
+              src="./assets/food.png"
+              alt="Foot dog"
+              width={70}
+              className=" mx-auto"
+            />
           </div>
 
           <div className="bg-gray-400 rounded-lg shadow-md p-4 text-center">
-            <p className="text-white font-bold py-2 px-4">
-              Paseos personales
-            </p>
-            <img src="./assets/menDogWalk.png" alt="men Dog Walk" width={45} className="mx-auto" />
+            <p className="text-white font-bold py-2 px-4">Paseos personales</p>
+            <img
+              src="./assets/menDogWalk.png"
+              alt="men Dog Walk"
+              width={45}
+              className="mx-auto"
+            />
           </div>
 
           <div className="bg-gray-400 rounded-lg shadow-md p-4 text-center">
-            <p className="text-white font-bold py-2 px-4">
-              Paseos sociales
-            </p>
-            <img src="./assets/walkWithDog.png" alt="walk with dog" width={65} className="mx-auto" />
+            <p className="text-white font-bold py-2 px-4">Paseos sociales</p>
+            <img
+              src="./assets/walkWithDog.png"
+              alt="walk with dog"
+              width={65}
+              className="mx-auto"
+            />
           </div>
 
           <div className="bg-gray-400 rounded-lg shadow-md p-4 text-center">
-            <p className="text-white font-bold py-2 px-4 " >
-              Veterinaria
-            </p>
-            <img src="./assets/veterinario.png" alt="Veterinaria" width={50} className="pt-2 mx-auto" />
+            <p className="text-white font-bold py-2 px-4 ">Veterinaria</p>
+            <img
+              src="./assets/veterinario.png"
+              alt="Veterinaria"
+              width={50}
+              className="pt-2 mx-auto"
+            />
           </div>
 
           <div className="bg-gray-400 rounded-lg shadow-md p-4 text-center">
-            <p className="text-white font-bold py-2 px-4" >
-              Actividades
-            </p>
-            <img src="./assets/playing.png" alt="Playing" width={70} className="mx-auto" />
+            <p className="text-white font-bold py-2 px-4">Actividades</p>
+            <img
+              src="./assets/playing.png"
+              alt="Playing"
+              width={70}
+              className="mx-auto"
+            />
           </div>
         </div>
 
         <div className="p-4 text-center">
           <p className="text-lg font-semibold text-center mt-4 mb-8">
-            Guarderías que cuidan cada detalle para que tu peludito se sienta en casa, donde podrán ser bañados, jugar con otras perritos, comer comida balanceada y pasear libremente.
+            Guarderías que cuidan cada detalle para que tu peludito se sienta en
+            casa, donde podrán ser bañados, jugar con otras perritos, comer
+            comida balanceada y pasear libremente.
           </p>
         </div>
 
@@ -121,7 +166,7 @@ const Home = () => {
             Reserva un lugar para tu peludito donde y cuando quieras
           </p>
           <div className="flex justify-center">
-            <form className="w-full max-w-md">
+            <form className="w-full max-w-md" onSubmit={handleSubmit}>
               <div className="flex flex-col items-center">
                 <input
                   className="input-field"
@@ -129,22 +174,24 @@ const Home = () => {
                   placeholder="Provincia, ciudad, localidad"
                   id="ciudad"
                   name="ciudad"
+                  onChange={(e) => {
+                    userSearch.place = e.target.value;
+                  }}
                 />
                 <select
                   className="input-field"
                   id="tipo-animal"
                   name="tipo-animal"
-                  value={tipoAnimal}
-                  onChange={(e) => setTipoAnimal(e.target.value)}
+                  onChange={(e) => (userSearch.petType = e.target.value)}
                 >
+                  <option value="">Seleccione una Mascota</option>
                   <option value="Perro">Perro</option>
                   <option value="Gato">Gato</option>
                   <option value="Otros">Otros</option>
                 </select>
-                <button 
+                <button
                   className="w-full max-w-lg py-4 md:mx-auto text-xl font-bold bg-amber-600 rounded-md"
                   type="submit"
-                  onClick={handleClick}
                 >
                   Buscar
                 </button>
--- frontend/src/components/pages/search/SearchPage.jsx
+++ frontend/src/components/pages/search/SearchPage.jsx
@@ -1,93 +1,12 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import ListResults from "./ListResults";
 import Filter from "./Filter";
-
-const branches = [
-  {
-    name: "Sucursal Canina Mi Sueño",
-    dirección: "Cachi 258, C1437 CABA",
-    ciudad: "Montevideo",
-    calificación: 5,
-    capacidadTotal: 20,
-    capacidadOcupada: 5,
-    reseñas: [
-      "excelente lugar",
-      "Mi perro hiper contento",
-      "No cambio más de lugar",
-      "Excelente atencion a mi gatito",
-    ],
-    servicios: [
-      "Peluquería",
-      "Dietas especiales",
-      "Juegos y actividades",
-      "Paseos personales",
-      "Paseos en manada",
-      "Veterinaria",
-    ],
-  },
-  {
-    name: "Sucursal Canina Dientitos",
-    dirección: "Belgrano 1043, C1437 CABA",
-    ciudad: "Paraná",
-    calificación: 3,
-    capacidadTotal: 10,
-    capacidadOcupada: 5,
-    reseñas: [
-      "Mi gatito volvió un poco nervioso",
-      "Me gustó mucho el lugar",
-      "Recontra Agredecidos por el servicio",
-      "No esta mal, mejoraría si pudieran ofrecer paseos",
-    ],
-    servicios: ["Peluquería", "Dietas especiales", "Juegos y actividades"],
-  },
-  {
-    name: "Sucursal Mascotas Guau!",
-    dirección: "Juan Manzonni 43, C1437 CABA",
-    ciudad: "Buenos Aires",
-    calificación: 4,
-    capacidadTotal: 10,
-    capacidadOcupada: 2,
-    reseñas: [
-      "Henry volvió re contento!",
-      "Me gustó mucho el lugar",
-      "Aparte de cuidarlo lo bañaron y quedo muy bien!",
-      "El precio es acorde al servicio",
-      "Me costó encontrar el lugar",
-      "Muy lindo lugar!",
-    ],
-    servicios: [
-      "Peluquería",
-      "Dietas especiales",
-      "Juegos y actividades",
-      "Paseos personales",
-    ],
-  },
-  {
-    name: "Sucursal Mascotas Su Reino de pelos",
-    dirección: "Paso de los Libres 800, C1437 CABA ",
-    ciudad: "La Plata",
-    calificación: 4,
-    capacidadTotal: 10,
-    capacidadOcupada: 2,
-    reseñas: [
-      "Henry volvió re contento!",
-      "Me gustó mucho el lugar",
-      "Aparte de cuidarlo lo bañaron y quedo muy bien!",
-      "El precio es acorde al servicio",
-      "Me costó encontrar el lugar",
-      "Muy lindo lugar!",
-    ],
-    servicios: [
-      "Peluquería",
-      "Dietas especiales",
-      "Juegos y actividades",
-      "Paseos personales",
-    ],
-  },
-];
+import { SearchContext } from "../../../context/SearchContext";
 
 const SearchPage = () => {
-  const [branchList, setBranchList] = useState(branches);
+  const [branchList, setBranchList] = useState([]);
+
+  const { searchTerm } = useContext(SearchContext);
 
   const updateBranches = (filteredBranches) => {
     setBranchList(filteredBranches);
@@ -96,7 +15,7 @@ const SearchPage = () => {
   return (
     <main>
       <Filter branchList={branchList} updateBranches={updateBranches} />
-      <ListResults branchList={branchList} />
+      <ListResults search={searchTerm} />
     </main>
   );
 };
