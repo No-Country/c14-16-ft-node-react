9c9f80b331e3a49565a3381df393b186752f6455
GuerreroEnier <guerreroenier@gmail.com>
2023-10-19 10:23:04 -0500

    modularizacion del home

--- frontend/src/components/pages/home/Home.jsx
+++ frontend/src/components/pages/home/Home.jsx
@@ -1,11 +1,11 @@
-
-
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SearchContext } from "../../../context/SearchContext";
-import DogHome from '/assets/Images/DogHome.jpeg'
-import StyledImage from './StyledImage';
+import DogHome from "/assets/Images/DogHome.jpeg";
+import StyledImage from "./StyledImage";
 import "./Home.css";
+
+import Hero from "../../hero";
 
 const Home = () => {
   const [place, setPlace] = useState("");
@@ -31,23 +31,21 @@ const Home = () => {
     setPet("");
     setPlace("");
     navigate("/search");
-
   };
 
-
   const imageStyles = {
-    background: '#f8d444',
+    background: "#f8d444",
   };
 
   const textStyles = {
-    color: '#000',
-    fontSize: '1.5em',
-    fontWeight: 'bold',
-    padding: '2px 4px',
+    color: "#000",
+    fontSize: "1.5em",
+    fontWeight: "bold",
+    padding: "2px 4px",
   };
 
   const boxStyle = {
-    background: '#f8d444',
+    background: "#f8d444",
   };
 
   const HeaderStyle = {
@@ -56,87 +54,25 @@ const Home = () => {
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
   };
-  const HeaderContainerStyle = {
-    height: "100vh",
-  };
 
   return (
     <>
       <header style={HeaderStyle}>
-        <div className="container mx-auto px-4 " style={HeaderContainerStyle}>
-          <h1 className="text-4xl text-white font-semibold text-center pt-5 mb-4">
-            Un lugar seguro para tu mascota
-          </h1>
-          <p className="text-2xl font-semibold text-center text-white mb-8">
-            Guarderias que cuidan cada detalle para que tu mascota se sienta en como en casa,
-            sin preocupaciones ni inquietudes. Aquí tendran un espacio para jugar, tambien tendran ricas comidas, jugaran y pasearan libremente.
-            con los mejores profesionales y servicios de calidad.
-          </p>
-        </div>
-        {/* Formulario de Búsqueda */}
-        <section className=" py-6 sm:py-8 lg:py-12">
-          <div className="bg-[rgba(224,224,224,0.55)] backdrop-blur-md rounded-lg p-6 text-center w-full sm:w-3/5 md:w-2/5 mx-auto mt-0">
-            <h2 className="text-lg font-semibold text-black mb-4">
-              Reserva un lugar para tu peludito donde y cuando quieras
-            </h2>
-            <div className="flex justify-center">
-              <form className="w-full max-w-md" onSubmit={handleSubmit}>
-                <div className="flex flex-col items-center">
-                  <input
-                    className="input-field"
-                    type="text"
-                    placeholder="Ingresa una localidad"
-                    id="ciudad"
-                    name="ciudad"
-                    required
-                    value={place}
-                    onChange={(e) => {
-                      //userSearch.place = e.target.value;
-                      setPlace(e.target.value);
-                    }}
-                  />
-                  <select
-                    className="input-field"
-                    id="tipo-animal"
-                    name="tipo-animal"
-                    required
-                    value={pet}
-                    onChange={(e) => {
-                      //userSearch.pet = e.target.value;
-                      setPet(e.target.value);
-                    }}
-                  >
-                    <option value="">Seleccione una Mascota</option>
-                    <option value="Perro">Perro</option>
-                    <option value="Gato">Gato</option>
-                    <option value="Otros">Otros</option>
-                  </select>
-                  <button
-                    className="w-full max-w-lg py-4 md:mx-auto text-xl font-bold bg-amber-600 rounded-md hover:bg-amber-400"
-                    type="submit"
-                  >
-                    Buscar
-                  </button>
-
-                </div>
-              </form>
-            </div>
-          </div>
-        </section>
+        <Hero />
       </header>
 
       <main className="bg-gray-100 py-8">
         <p className="text-2xl font-semibold text-center text-black mb-8">
-          En Doggy&#39;s queremos que estes tranquilo y confiado de que
-          tu mascota está en buenas manos con nosotros. Por eso, queremos
-          contarte un poco sobre lo que tenemos para ofrecer.
+          En Doggy&#39;s queremos que estes tranquilo y confiado de que tu
+          mascota está en buenas manos con nosotros. Por eso, queremos contarte
+          un poco sobre lo que tenemos para ofrecer.
         </p>
         <div className="flex flex-col md:flex-row m-4 justify-center items-center gap-6">
           {/* Box 1 */}
           <div
             className="md:w-1/3 bg-gray-400 rounded-lg shadow-md p-4 text-center"
             style={{
-              background: '#f8d444',
+              background: "#f8d444",
             }}
           >
             <img
@@ -156,7 +92,7 @@ const Home = () => {
           <div
             className="md:w-1/3 bg-gray-400 rounded-lg shadow-md p-4 text-center"
             style={{
-              background: '#f8d444',
+              background: "#f8d444",
             }}
           >
             <img
@@ -177,7 +113,7 @@ const Home = () => {
           <div
             className="md:w-1/3 bg-gray-400 rounded-lg shadow-md p-4 text-center"
             style={{
-              background: '#f8d444',
+              background: "#f8d444",
             }}
           >
             <img
@@ -197,10 +133,15 @@ const Home = () => {
 
         {/* Servicios  */}
 
-        <h2 className="text-4xl text-black font-bold text-center pt-5 mb-4">Servicios</h2>
+        <h2 className="text-4xl text-black font-bold text-center pt-5 mb-4">
+          Servicios
+        </h2>
         <div className="flex flex-col md:flex-row m-4 justify-center items-center gap-6">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            <div className="bg-gray-400 rounded-lg p-4 text-center" style={boxStyle}>
+            <div
+              className="bg-gray-400 rounded-lg p-4 text-center"
+              style={boxStyle}
+            >
               <StyledImage
                 imageSrc="/assets/Images/Peluqueria.png"
                 imageStyle={imageStyles}
@@ -209,7 +150,10 @@ const Home = () => {
               />
             </div>
 
-            <div className="bg-gray-400 rounded-lg  p-4 text-center" style={boxStyle}>
+            <div
+              className="bg-gray-400 rounded-lg  p-4 text-center"
+              style={boxStyle}
+            >
               <StyledImage
                 imageSrc="/assets/Images/PaseosIndividuales.png"
                 imageStyle={imageStyles}
@@ -218,7 +162,10 @@ const Home = () => {
               />
             </div>
 
-            <div className="bg-gray-400 rounded-lg  p-4 text-center" style={boxStyle}>
+            <div
+              className="bg-gray-400 rounded-lg  p-4 text-center"
+              style={boxStyle}
+            >
               <StyledImage
                 imageSrc="/assets/Images/PaseosGrupales.png"
                 imageStyles={imageStyles}
@@ -227,7 +174,10 @@ const Home = () => {
               />
             </div>
 
-            <div className="bg-gray-400 rounded-lg  p-4 text-center" style={boxStyle}>
+            <div
+              className="bg-gray-400 rounded-lg  p-4 text-center"
+              style={boxStyle}
+            >
               <StyledImage
                 imageSrc="/assets/Images/Veterinario.png"
                 imageStyles={imageStyles}
@@ -235,7 +185,10 @@ const Home = () => {
                 text="Veterinario"
               />
             </div>
-            <div className="bg-gray-400 rounded-lg  p-4 text-center" style={boxStyle}>
+            <div
+              className="bg-gray-400 rounded-lg  p-4 text-center"
+              style={boxStyle}
+            >
               <StyledImage
                 imageSrc="/assets/Images/DietasEspeciales.png"
                 imageStyles={imageStyles}
@@ -244,7 +197,10 @@ const Home = () => {
               />
             </div>
 
-            <div className="bg-gray-400 rounded-lg  p-4 text-center" style={boxStyle}>
+            <div
+              className="bg-gray-400 rounded-lg  p-4 text-center"
+              style={boxStyle}
+            >
               <StyledImage
                 imageSrc="/assets/Images/JuegosActividades.png"
                 imageStyles={imageStyles}
--- frontend/src/components/ui/select.jsx
+++ frontend/src/components/ui/select.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
-function Select({ name, values, options }) {
+function Select({ name, values, options, onChange }) {
   return (
     <>
       <select
         name={name}
         id={name}
-        className="bg-primary/50 px-4 py-3 border rounded-lg focus:outline-none focus:border-primary text-gray-600 font-medium outline-none"
+        className="bg-primary/50 px-4 py-3 border rounded-lg focus:outline-none focus:border-primary text-gray-600 font-medium outline-none w-full"
+        onChange={onChange}
       >
         {values.map((val, index) => (
           <option key={index} value={val}>
@@ -21,5 +22,6 @@ Select.propTypes = {
   name: PropTypes.string.isRequired,
   values: PropTypes.array.isRequired,
   options: PropTypes.array.isRequired,
+  onChange: PropTypes.func,
 };
 export default Select;
