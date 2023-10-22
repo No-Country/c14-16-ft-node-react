bcc86b50bf0fdc22a43393382a1304c286b7e5a5
Emanuel Valente <emavalente.dev@gmail.com>
2023-10-22 19:39:36 -0300

    22/10/2023 - Trabajo en llamado GET a la api para la búsqueda principal y estilos de la búsqueda y la card

--- frontend/src/components/pages/search/Filter.jsx
+++ frontend/src/components/pages/search/Filter.jsx
@@ -1,20 +1,24 @@
-
-
 import "./Filter.css";
+import scissors from "../../../../public/assets/icons/scissors.svg";
+import foot from "../../../../public/assets/icons/chicken-leg.svg";
+import walkplus from "../../../../public/assets/icons/image 59.svg";
+import walk from "../../../../public/assets/icons/image 61.svg";
+import stick from "../../../../public/assets/icons/wood-stick.svg";
+import vet from "../../../../public/assets/icons/map_veterinary-care.svg";
 
 const Filter = () => {
   return (
-    <div className="container mx-auto p-8 bg-[#F0A225] sm:rounded-xl">
-      <h2 className="mb-8 text-xl text-center text-white font-bold">
-        Filtros de búsqueda
+    <div className="container mx-auto p-8 bg-[white] shadow-lg sm:rounded-xl">
+      <h2 className="my-8 text-xl text-center text-black font-bold">
+        Filtro de búsqueda
       </h2>
-      <form id="filter-form" className="flex flex-col items-center">
-        <div className="w-[70%]">
-          <h3 className="mb-4 text-md text-white font-bold">
+      <form id="filter-form" className="w-full flex flex-col items-center">
+        <div className="w-full lg:w-[70%]">
+          <h3 className="mb-4 text-md text-black font-bold">
             Servicios Disponibles
           </h3>
           <div className="mb-8 grid grid-cols-2 gap-4 md:flex md:flex-wrap md:flex-row ">
-            <label htmlFor="peluqueria">
+            <label htmlFor="peluqueria" className="flex items-center">
               <input
                 id="peluqueria"
                 type="checkbox"
@@ -22,8 +26,9 @@ const Filter = () => {
                 className="mr-2"
               />
               Peluquería
+              <img src={scissors} alt="icono de tijeras" className="w-6 ml-2" />
             </label>
-            <label htmlFor="dieta">
+            <label htmlFor="dieta" className="flex items-center">
               <input
                 id="dieta"
                 type="checkbox"
@@ -31,8 +36,9 @@ const Filter = () => {
                 className="mr-2"
               />
               Dietas especiales
+              <img src={foot} alt="icono de comida" className="w-6 ml-2" />
             </label>
-            <label htmlFor="actividades">
+            <label htmlFor="actividades" className="flex items-center">
               <input
                 id="actividades"
                 type="checkbox"
@@ -40,8 +46,9 @@ const Filter = () => {
                 className="mr-2"
               />
               Juegos y actividades
+              <img src={stick} alt="icono de rama" className="w-6 ml-2" />
             </label>
-            <label htmlFor="paseo-personal">
+            <label htmlFor="paseo-personal" className="flex items-center">
               <input
                 id="paseo-personal"
                 type="checkbox"
@@ -49,8 +56,9 @@ const Filter = () => {
                 className="mr-2"
               />
               Paseos personales
+              <img src={walk} alt="icono de paseos" className="w-6 ml-2" />
             </label>
-            <label htmlFor="veterinaria">
+            <label htmlFor="veterinaria" className="flex items-center">
               <input
                 id="veterinaria"
                 type="checkbox"
@@ -58,8 +66,9 @@ const Filter = () => {
                 className="mr-2"
               />
               Veterinaria
+              <img src={vet} alt="icono de veterinaria" className="w-6 ml-2" />
             </label>
-            <label htmlFor="paseo-manada">
+            <label htmlFor="paseo-manada" className="flex items-center">
               <input
                 id="paseo-manada"
                 type="checkbox"
@@ -67,16 +76,20 @@ const Filter = () => {
                 className="mr-2"
               />
               Paseos en manada
+              <img src={walkplus} alt="icono de paseos" className="w-6 ml-2" />
             </label>
           </div>
         </div>
-        <div className="w-[70%] mb-16 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="w-full lg:w-[70%] mb-16 grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
-            <h3 className="mb-4 text-md text-white font-bold">
+            <h3 className="mb-4 text-md text-black font-bold">
               Tipo de mascota
             </h3>
             <div>
-              <select type="select" className="w-full">
+              <select
+                type="select"
+                className="w-full p-4 bg-gray-100 border-b-2 border-[#333] rounded-md outline-none"
+              >
                 <option value="">Selecciona una mascota</option>
                 <option value="">Perro Grande</option>
                 <option value="">Perro Mediano</option>
@@ -87,9 +100,12 @@ const Filter = () => {
             </div>
           </div>
           <div>
-            <h3 className="mb-4 text-md text-white font-bold">Ciudad</h3>
+            <h3 className="mb-4 text-md text-black font-bold">Ciudad</h3>
             <div>
-              <select type="select" className="w-full">
+              <select
+                type="select"
+                className="w-full p-4 bg-gray-100 border-b-2 border-[#333] rounded-md outline-none"
+              >
                 <option value="">Selecciona tu Ciudad</option>
                 <option value="Buenos Aires">Buenos Aires</option>
                 <option value="Parana">Paraná</option>
@@ -99,7 +115,7 @@ const Filter = () => {
             </div>
           </div>
         </div>
-        <button className="w-full max-w-lg py-4 md:mx-auto text-xl font-bold bg-amber-600 rounded-md">
+        <button className="w-full max-w-lg py-4 md:mx-auto text-xl text-white font-bold bg-primary rounded-md">
           Buscar
         </button>
       </form>
--- frontend/src/components/pages/search/SearchPage.jsx
+++ frontend/src/components/pages/search/SearchPage.jsx
@@ -1,14 +1,51 @@
 import ListBranches from "./ListBranches";
 import Filter from "./Filter";
+import { useFetch } from "../../../customHooks/useFetch.js";
+import { useContext, useEffect } from "react";
+import { SearchContext } from "../../../context/SearchContext";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const SearchPage = () => {
+  const { searchTerm } = useContext(SearchContext);
+
+  const { data, loading, errorFetch } = useFetch(
+    "https://doggyhouse.azurewebsites.net/api/branches"
+  );
+
+  const MainLoading = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "30%",
+  };
+
+  console.log(searchTerm);
+
   return (
-    <main>
-      <Filter />
-      <ListBranches />
-    </main>
+    <>
+      {loading ? (
+        <main className="flex flex-col justify-center items-center py-[30%] md:py-[35%] lg:py-[24%] xl:py-[14%]">
+          <ClipLoader
+            loading={loading}
+            color="#f8ce25"
+            cssOverride={{}}
+            size={54}
+          />
+          <p>Cargando sucursales...</p>
+        </main>
+      ) : !loading && !errorFetch ? (
+        <main>
+          <Filter />
+          <ListBranches branches={data.result} />
+        </main>
+      ) : (
+        <main>
+          <p>{errorFetch.error.message}</p>
+        </main>
+      )}
+    </>
   );
 };
 
 export default SearchPage;
-
