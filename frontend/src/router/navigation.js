e5a05163eccdbc2f9943ae71c3ec918c7b6ef786
Rolando Duarte <rolandoduarte83@gmail.com>
2023-10-25 18:54:00 -0300

    cambios con login

---
+++ frontend/src/router/navigation.js
@@ -0,0 +1,18 @@
+import AiTwotoneHome from 'react-icons/ai';
+import FcAbout from 'react-icons/fc';
+
+
+export const menuItems = [
+    {
+        id: "home",
+        path: "/",
+        title: "Inicio",
+        Icon: AiTwotoneHome
+    },
+    {
+        id: "about",
+        path: "/about",
+        title: "Nosotros",
+        Icon: FcAbout
+    }
+]
