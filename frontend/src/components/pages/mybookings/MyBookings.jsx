5ea1b23a2eb434db894e307159e5241d78445909
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-11-02 11:58:53 -0300

    bookings with map

--- frontend/src/components/pages/mybookings/MyBookings.jsx
+++ frontend/src/components/pages/mybookings/MyBookings.jsx
@@ -1,8 +1,26 @@
 import BookCard from "./BookCard.jsx";
+import { TOKEN_KEY } from "../../../constants/api.js";
+import { useEffect, useState } from "react";
 
 const MyBookings = () => {
+  const user = JSON.parse(localStorage.getItem("User"));
+  const token = localStorage.getItem(TOKEN_KEY);
+  const [bookings, setBookings] = useState([])
+
+
+  useEffect(() => {
+    const getData = async() => {
+      const response = await fetch(`https://doggyhouse.azurewebsites.net/api/bookings/byUser/${user.id}`, { headers: { Authorization: `Bearer ${token}` } })
+      const responseData = await response.json();
+      setBookings(responseData.result)
+    }
+
+    getData()
+  }, [token, user.id])
+
   return (
     <div className="container mx-auto py-4 ">
+      {bookings.length !== 0 ?
       <table className="w-full">
         <thead className="text-white font-bold font-roboto bg-primary">
           <tr>
@@ -13,11 +31,13 @@ const MyBookings = () => {
           </tr>
         </thead>
         <tbody>
-          <BookCard />
-          <BookCard />
-          <BookCard />
+          {bookings.map((booking) =>(
+            <BookCard key={booking.id} booking={booking} />
+          ))}
         </tbody>
       </table>
+      :
+      <div>No hay datos</div>}
     </div>
   );
 };
